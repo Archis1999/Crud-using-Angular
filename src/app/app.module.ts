27322580cbecaf1511cd6969e5fe73f6b89708f4
archisDeshmukh <archisd80@gmail.com>
2022-06-30 11:04:44 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CrudBooksComponent } from './crud-books/crud-books.component';
 
@@ -11,6 +11,7 @@ import { CrudBooksComponent } from './crud-books/crud-books.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
